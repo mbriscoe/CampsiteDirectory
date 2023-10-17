7750df27e9912b4b9d02f17f4128ec243e7827b8
Mark Briscoe <mark@mcmw.co.uk>
2023-10-17 11:43:50 +0100

    up to end of section 52

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+const Review = require("../models/review");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
@@ -15,11 +16,13 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
+    await Review.deleteMany({});
 
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: "652e4d47b7be43ce49cb03dd",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: "https://picsum.photos/500/400",
