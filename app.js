fccbd2662aa39b2cbe9af787b2b05d1f7027718e
Mark Briscoe <mark@mcmw.co.uk>
2023-10-28 17:55:09 +0100

    added random 'days ago' number to foot of show card

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
 }
 
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const flash = require("connect-flash");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const userRoutes = require("./routes/users");
-const campgroundRoutes = require("./routes/campgrounds");
-const reviewRoutes = require("./routes/reviews");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
-const MongoDBStore = require("connect-mongo")(session);
+const MongoDBStore = require('connect-mongo')(session);
 
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+let dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
+
+if (dbUrl === 'mongodb://127.0.0.1:27017/yelp-camp') {
+    dbConnectionMessage = 'Database connected: Locally';
+} else {
+    dbConnectionMessage = 'Database connected: Remotely';
+}
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -31,26 +37,26 @@ mongoose.connect(dbUrl, {
 });
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected: ", dbUrl);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log(dbConnectionMessage);
 });
 
 const app = express();
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     mongoSanitize({
-        replaceWith: "_",
+        replaceWith: '_',
     })
 );
-const secret = process.env.SECRET || "thisshouldbeabettersecret!";
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 
 const store = new MongoDBStore({
     url: dbUrl,
@@ -58,13 +64,13 @@ const store = new MongoDBStore({
     touchAfter: 24 * 60 * 60,
 });
 
-store.on("error", function (e) {
-    console.log("SESSION STORE ERROR", e);
+store.on('error', function (e) {
+    console.log('SESSION STORE ERROR', e);
 });
 
 const sessionConfig = {
     store,
-    name: "session",
+    name: 'session',
     secret,
     resave: false,
     saveUninitialized: true,
@@ -81,24 +87,29 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://api.mapbox.com/',
+    'https://kit.fontawesome.com/',
+    'https://cdnjs.cloudflare.com/',
+    'https://cdn.jsdelivr.net',
 ];
 
 const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
+    'https://kit-free.fontawesome.com/',
+    'https://stackpath.bootstrapcdn.com/',
+    'https://api.mapbox.com/',
+    'https://api.tiles.mapbox.com/',
+    'https://fonts.googleapis.com/',
+    'https://use.fontawesome.com/',
 ];
 
-const connectSrcUrls = ["https://api.mapbox.com/", "https://a.tiles.mapbox.com/", "https://b.tiles.mapbox.com/", "https://events.mapbox.com/"];
+const connectSrcUrls = [
+    'https://api.mapbox.com/',
+    'https://a.tiles.mapbox.com/',
+    'https://b.tiles.mapbox.com/',
+    'https://events.mapbox.com/',
+];
 
 const fontSrcUrls = [];
 
@@ -109,14 +120,14 @@ app.use(
             connectSrc: ["'self'", ...connectSrcUrls],
             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
+            workerSrc: ["'self'", 'blob:'],
             objectSrc: [],
             imgSrc: [
                 "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dqe5yeugm/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
-                "https://images.unsplash.com/",
+                'blob:',
+                'data:',
+                'https://res.cloudinary.com/dqe5yeugm/', //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                'https://images.unsplash.com/',
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
         },
@@ -132,27 +143,27 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 });
 
-app.use("/", userRoutes);
-app.use("/campgrounds", campgroundRoutes);
-app.use("/campgrounds/:id/reviews", reviewRoutes);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
-app.get("/", (req, res) => {
-    res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found", 404));
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 });
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = "Oh No, Something Went Wrong!";
-    res.status(statusCode).render("error", { err });
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 const port = process.env.PORT || 3000;
